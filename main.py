b41696f286cd2788ddafe23361a38526e530c3b5
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-08-05 20:25:32 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+
+image = cv2.imread('sunset.png')
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+height, width, _ = image_rgb.shape
+
+unique_colors = set()
+for y in range(height):
+    for x in range(width):
+        pixel_color = tuple(image_rgb[y, x])
+        unique_colors.add(pixel_color)
+
+  print(len(unique_colors))
